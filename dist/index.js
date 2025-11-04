db5ae7de24882d5569f2abee9c775c99e7ff5024
bachvd <bach.vu@icomm-group.com>
2025-11-04 17:24:05 +0700

    bachvd

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+}));
+app.options('*', cors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Routes
